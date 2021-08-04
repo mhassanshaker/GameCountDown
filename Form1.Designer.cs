0dd22da831cc6b2ca1a6b5387f469c189632b787
mhassanshaker <85250473+mhassanshaker@users.noreply.github.com>
2021-08-04 18:13:22 +0400

    Initial

---
+++ Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace GameCountDown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Countdown = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Countdown
+            // 
+            this.Countdown.AutoSize = true;
+            this.Countdown.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Countdown.Location = new System.Drawing.Point(24, 25);
+            this.Countdown.Name = "Countdown";
+            this.Countdown.Size = new System.Drawing.Size(79, 29);
+            this.Countdown.TabIndex = 0;
+            this.Countdown.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 196);
+            this.Controls.Add(this.Countdown);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Countdown;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameCountDown
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            new System.Threading.Thread(new System.Threading.ThreadStart(doCountDown)).Start();
+                  
+        }
+        void doCountDown()
+        {
+            for (int i = 3; i > 0; i--)
+            {
+                setCountDownText( "GAME STARTS IN: " + i);
+                System.Threading.Thread.Sleep(1000);
+            }
+            MessageBox.Show("Start Play");
+        }
+        void setCountDownText(string txtValue)
+        {
+            if (Countdown.InvokeRequired)
+            {
+                Action safeWrite = delegate { setCountDownText(txtValue); };
+                Countdown.Invoke(safeWrite);
+            }
+            else
+                Countdown.Text = txtValue;
+        }
+    }
+}
